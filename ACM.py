9db962e8ae5c6dce615666952b8bf7640525e453
lushl9301 <lushl9301@gmail.com>
2015-06-30 10:22:39 +0800

    add age=0. never fetch again

--- ACM.py
+++ ACM.py
@@ -19,16 +19,16 @@ class Handler(BaseHandler):
     
     def on_start(self):
         self.crawl('http://dl.acm.org/citation.cfm?id=1571941.1572041&coll=DL&dl=GUIDE&CFID=514335100&CFTOKEN=57665532',
-                   callback=self.detail_page)
+                   age=0, callback=self.detail_page)
         self.crawl('http://dl.acm.org/results.cfm?h=1&cfid=514335100&cftoken=57665532',
                    age=0, callback=self.index_page)
         
     def index_page(self, response):
         for each in response.doc('a').items():
             if re.match('http://dl\.acm\.org\/citation\.cfm\?id=.+CFTOKEN.+', each.attr.href):
-                self.crawl(each.attr.href, callback=self.detail_page)
+                self.crawl(each.attr.href, age=0, callback=self.detail_page)
             if re.match('http://dl\.acm\.org\/results\.cfm\?query=.+$', each.attr.href):
-                self.crawl(each.attr.href, callback=self.index_page)
+                self.crawl(each.attr.href, age=0, callback=self.index_page)
         
     @config(fetch_type="js")
     def detail_page(self, response):
